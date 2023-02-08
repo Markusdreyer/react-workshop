0ae12311ee73efae59e6337e4712fbbba7279514
jacksy <jacksy@elkjop.no>
2023-02-08 15:40:39 +0100

    Updates app.tsx to correct stage

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 
 function App() {
-
-    const getRecipe = () => {
-            console.log("Hello world")
-        }
-
-        return (
-            <Box sx={{textAlign: "center", justifyContent: "center", padding: 2}}>
-                <Box sx={{fontSize: 26, paddingBottom: 2}}>
-                YourName's Magic Cookbook
-                </Box>
-                <Button variant={'outlined'} onClick={() => getRecipe()}>Get Recipe</Button>
-            </Box>
-        );
+  
+  const getRecipe = async () => {
+    const requestBody = JSON.stringify({ingredients: ["tomato", "mozzarella", "basil", "chiocciole pasta", "olive oil"]})
+    await fetch("http://localhost:8000/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  
+  return (
+      <Box sx={{textAlign: "center", justifyContent: "center", padding: 2}}>
+          <Box sx={{fontSize: 26, paddingBottom: 2}}>
+          YourName's Magic Cookbook
+          </Box>
+          <Button variant={'outlined'} onClick={() => getRecipe()}>Get Recipe</Button>
+      </Box>
+  );
 }
-
 export default App;
