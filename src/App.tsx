a454665dec63d875662f65fdc6f75aac8cd6d3a5
jack-syv <123395061+jack-syv@users.noreply.github.com>
2023-02-14 12:51:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 
 function App() {
   
-  async function getRecipe {
+  async function getRecipe() {
     const requestBody = JSON.stringify({
         ingredients: [
             "tomato", 
